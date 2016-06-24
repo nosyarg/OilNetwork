d1e2d29edfb5d3eb352d90dc4d6dd2d96a833497
nosyarg <york17a@ncssm.edu>
2016-06-24 13:12:42 -0400

    started matlab coding

--- testscript.m
+++ testscript.m
@@ -1,1 +1,13 @@
-companies = csvread('25biggestformatlabcpy.csv');
+companies = csvread('25biggestformatlabcpy.csv'); %Pull in my big company dataset
+connections = csvread('bp2015oilnetworkformatlab.csv'); %Pull in the adjacency matrix from bp
+connections = connections > 3; %find a minimum threshhold for connections
+Gupper = graph(connections,'upper');%plot some graphs which are not particularly meaningful other than showing collaboration
+plot(Gupper);
+Glower = graph(connections,'lower');
+plot(Glower);
+countrytogdprank = containers.Map();
+[id, country, GDP] = csvimport('GDPByCountryformatlab.csv', 'columns',  {'Ranking', 'Economy','Size'});
+for c = 1:length(country)
+    current = country(c);
+    countrytogdprank(char(current)) = id(c);
+end
